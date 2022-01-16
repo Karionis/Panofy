ca81c612fc5cb6db74cda2148bd2078489f0f41c
Karionis <niklas.kanthak@online.de>
2022-01-16 17:45:53 +0100

    Added Calculator.cs

---
+++ Panofy/Calculator.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panofy
+{
+  public class Calculator
+  {
+    public static int Add(int x, int y)
+    {
+      return x + y;
+    }
+    public static double Subtract(double x, double y)
+    {
+      return x - y;
+    }
+    public static double Multiply(double x, double y)
+    {
+      return x * y;
+    }
+    public static int Division(int x, int y)
+    {
+      return x / y;
+    }
+    public static double Division(double x, double y)
+    {
+      return x / y;
+    }
+  }
+}
