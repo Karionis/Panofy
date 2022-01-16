a00b330df51d6f721f1e11a80959215d98c8ba2d
Karionis <niklas.kanthak@online.de>
2022-01-16 20:01:41 +0100

    Added program body

--- Panofy/Program.cs
+++ Panofy/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Panofy;
+
+namespace Panofy
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            var converter = new FahrenheitToCelsius();
+            var celsius = converter.FahrenheitToCelsiusConverter(212.0);
+            Console.WriteLine($"Fahrenheit temp of 212 is {celsius}.");
+        }
+    }
+}
